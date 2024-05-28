ec031d462e6829ea1afe3900313e3fa58cf8617c
Yash-Boldhane <yashboldhane99@gmail.com>
2024-05-28 13:05:30 +0530

    ..

---
+++ Object Oriented Programming/Method Overloading/Main.java
@@ -0,0 +1,27 @@
+class Main
+{
+	public  static void main(String[]args)
+	{
+		System.out.println(main(100.0,10.0));
+	}
+	
+	public static int main(int a, int b)
+	{
+		return a-b;
+	}
+
+	public static float main(int a, float b)
+	{
+		return a-b;
+	}
+	
+	public static double main(double a, double b)
+	{
+		return a-b;
+	}
+
+
+	
+
+	
+}
