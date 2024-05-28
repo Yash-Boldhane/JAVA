4bc21b74f1a0d7a825e482b94650d12dfa95b847
Yash-Boldhane <yashboldhane99@gmail.com>
2024-05-28 12:25:37 +0530

    changes

---
+++ Object Oriented Programming/Constructors/Constructor Overloading/laptopDriver.java
@@ -0,0 +1,8 @@
+class laptopDriver
+{
+	public static void main(String[]args)	
+	{
+		laptop l1=new laptop("HP","8GB","Yes",59999.00,"No");
+		l1.displaylaptop();
+	}
+}
---
+++ Object Oriented Programming/Method Overloading/Calculator.java
@@ -0,0 +1,30 @@
+class Calculator
+{
+	public static void main(String[]args)
+	{
+		System.out.println(add(199.25,1.0));
+	}
+	public static int add(int a, int b)
+	{
+		return a+b;
+	}
+
+	public static float add(float a,float b)
+	{
+		return a+b;
+	}
+
+
+	public static double add(double a, double b)
+	{
+		return a+b;
+	}
+
+		public static double add( int a, double b)
+	{
+		return a+b;
+	}
+
+
+
+}
